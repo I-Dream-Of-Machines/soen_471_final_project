070dd224a54ac3d35a21a91f03efaf38f2d2c37f
Nadia Sheikh <n_she@encs.concordia.ca>
2021-04-17 13:33:25 -0400

    copied two additional functions from Shahrareh's code

--- code/regression_utilities.py
+++ code/regression_utilities.py
@@ -42,5 +42,42 @@ def print_save_metrics(y_test, y_pred, ov, metric_file_path):
     print('Root Mean Squared Error (RMSE):' + ov, rmse)
 
 
+def feature_importance(important_list):
+    lst_column_map = init_column_maps()
+    lst_final = []
+    # important_list=sorted(list(zip(regressor.feature_importances_,X_test.columns)),key =lambda x: x[0] ,reverse=True)[:10]
+    for import_item in important_list:
+        import_item_rem = import_item[1].replace('_imputed', '')
+        for column_item in lst_column_map:
+            if import_item_rem == column_item[0]:
+                import_renamed = column_item[2]
+                l = list(import_item)
+                l[1] = import_renamed
+                lst_final.append(tuple(l))
+                break
+    return lst_final
+
+
+
+def random_forest():
+    output_variable = ['School_Code', 'OP1', 'OP2', 'OP6', 'OP3', 'OP4', 'OP5', 'OP7', 'OP8', 'OP9', 'OP10', 'OP11',
+                       'OP12', 'OP13', 'OP14']
+    output_variable.remove('School_Code')
+    for item in output_variable:
+        X_train, X_test, y_train, y_test = train_test_split(item)
+        regressor = RandomForestRegressor()
+        regressor.fit(X_train, y_train)
+        y_pred = regressor.predict(X_test)
+        with open('../data/random_forest/' + item + '.pkl', 'wb') as pickle_file:
+            pk.dump(y_pred, pickle_file)
+
+        important_list = sorted(list(zip(regressor.feature_importances_, X_test.columns)), key=lambda x: x[0],
+                                reverse=True)[:10]
+        import_lst = feature_importance(important_list)
+        file = open('../data/random_forest/feature_importance_' + item + '.csv', 'w+', newline='')
+        with file:
+            write = csv.writer(file)
+            write.writerows(import_lst)
+        print_save_metrics(y_test, y_pred, item)
 
 
